566c5899e2e2baddd0c5bda3063f25a7f6ad8370
Danielle Thompson <danithompson74@gmail.com>
2020-10-23 12:44:59 -0700

    Try to fix list bug by changing click elements from class to ID - still bugged

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $("p").click(function() {
-    $(".show-uses1").show();
-    $(".show-uses2").show();
-    $(".show-uses3").show();
-    $(".show-uses4").show();
-    $(".show-uses5").show();
-    $(".show-uses6").show();
+    $("#show-uses1").show();
+    $("#show-uses2").show();
+    $("#show-uses3").show();
+    $("#show-uses4").show();
+    $("#show-uses5").show();
+    $("#show-uses6").show();
   });
 });
