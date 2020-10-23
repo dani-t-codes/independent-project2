c31b2d1c0fe40574b0812f1ca559aba355fb0448
Danielle Thompson <danithompson74@gmail.com>
2020-10-23 15:39:42 -0700

    fix spacing error on styles page, link image into quiz results section, add page link backslash according to color scheme, build out question form submission in scripts - still buggy

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
   $("p").click(function() {
     $("#show-uses1").toggle();
-    $("#show-uses2").toggle();
-    $("#show-uses3").toggle();
-    $("#show-uses4").toggle();
-    $("#show-uses5").toggle();
-    $("#show-uses6").toggle();
   });
 });
 
 $(document).ready(function() {
-  $("#quiz").submit(function) {
-    const question1Input = parseInt($("#question1").val();
+  $("#quiz").submit(function() {
+    const question1Input = $("#question1").val();
+
+    $("#question1").parseInt(question1Input);
+
+    $("#quiz-results").show();
 
     event.preventDefault();
-  }
+  });
+});
+
+$(document).ready(function() {
+  $("")
 }
