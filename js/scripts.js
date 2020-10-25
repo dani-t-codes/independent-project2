f5e82af090953b6c3ae9354bd294487aabb6b8a6
Danielle Thompson <danithompson74@gmail.com>
2020-10-25 15:33:12 -0700

    Fix bug that allows multiple results to come through for form submission, update results styling when they do show, and add a link in the results to go back to the quiz page

--- js/scripts.js
+++ js/scripts.js
@@ -44,16 +44,16 @@ $(document).ready(function() {
 //this line of code is for totalling up value inputs once I can figure out how to do that. Must parse value for accurate result!
 //    $("#question1").parseInt(question1Input);
     if (question1 === "1") {
-      $("#swift-logo").removeClass();
+      $(this).remove();
       $("#swift-logo").fadeIn();
     } else if (question1 === "2") {
-      $('#java-logo').removeClass();
+      $(this).remove();
       $('#java-logo').fadeIn();
     } else if (question1 === "3") {
-      $('#javascript-logo').removeClass();
+      $(this).remove();
       $('#javascript-logo').fadeIn();
     } else if (question1 === "4") {
-      $('#python-logo').removeClass();
+      $(this).remove();
       $('#python-logo').fadeIn();
     }
     
@@ -66,16 +66,16 @@ $(document).ready(function() {
     const question2 = $("select#question2").val();
 
     if (question2 === "1") {
-      $("#swift-logo").removeClass();
+      $(this).remove();
       $("#swift-logo").fadeIn();
     } else if (question2 === "2") {
-      $('#java-logo').removeClass();
+      $(this).remove();
       $('#java-logo').fadeIn();
     } else if (question2 === "3") {
-      $('#javascript-logo').removeClass();
+      $(this).remove();
       $('#javascript-logo').fadeIn();
     } else if (question2 === "4") {
-      $('#python-logo').removeClass();
+      $(this).remove();
       $('#python-logo').fadeIn();
     }
     
@@ -88,16 +88,16 @@ $(document).ready(function() {
     const question3 = $("select#question3").val();
 
     if (question3 === "1") {
-      $("#swift-logo").removeClass();
+      $(this).remove();
       $("#swift-logo").fadeIn();
     } else if (question3 === "2") {
-      $('#java-logo').removeClass();
+      $(this).remove();
       $('#java-logo').fadeIn();
     } else if (question3 === "3") {
-      $('#javascript-logo').removeClass();
+      $(this).remove();
       $('#javascript-logo').fadeIn();
     } else if (question3 === "4") {
-      $('#python-logo').removeClass();
+      $(this).remove();
       $('#python-logo').fadeIn();
     }
     
@@ -110,16 +110,16 @@ $(document).ready(function() {
     const question4 = $("select#question4").val();  
 
     if (question4 === "1") {
-      $("#swift-logo").removeClass();
+      $(this).remove();
       $("#swift-logo").fadeIn();
     } else if (question4 === "2") {
-      $('#java-logo').removeClass();
+      $(this).remove();
       $('#java-logo').fadeIn();
     } else if (question4 === "3") {
-      $('#javascript-logo').removeClass();
+      $(this).remove();
       $('#javascript-logo').fadeIn();
     } else if (question4 === "4") {
-      $('#python-logo').removeClass();
+      $(this).remove();
       $('#python-logo').fadeIn();
     }
     
@@ -127,21 +127,21 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-  $("form#quiz").submit(function(event) {
+  $("form#quiz").first().submit(function(event) {
     event.preventDefault();
     const question5 = $("select#question5").val();
 
     if (question5 === "1") {
-      $('body').removeClass();
-      $("#swift-logo").fadeIn();
+      $(this).remove();
+      $('#swift-logo').fadeIn();
     } else if (question5 === "2") {
-      $('body').removeClass();
+      $(this).remove();
       $('#java-logo').fadeIn();
     } else if (question5 === "3") {
-      $('body').removeClass();
+      $(this).remove();
       $('#javascript-logo').fadeIn();
     } else if (question5 === "4") {
-      $('body').removeClass();
+      $(this).remove();
       $('#python-logo').fadeIn();
     }
     
