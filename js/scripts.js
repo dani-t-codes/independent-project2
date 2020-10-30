fe1d8d07f728006c117b5e36326fbe5b24af5454
Danielle Thompson <danithompson74@gmail.com>
2020-10-29 20:39:25 -0700

    Move all pseudocode and testing code to testing.js, complete the branching for question 1

--- js/scripts.js
+++ js/scripts.js
@@ -32,51 +32,19 @@ $(document).ready(function() {
 
   $("form#quiz").submit(function(event) {
     event.preventDefault();
-    const result1 = $("#question1").val();
-    const result2 = $("#question2").val();
-    const result3 = $("#question3").val();
-    const result4 = $("#question4").val(); 
-    const result5 = $("#question5").val();
-
-    let val1Swift = [];
-    result1.forEach(function(element) {
-      // (if result1 === 1  val1Swift.push() 
-      // else if result1 === 2 val2Java.push()
-      //else if result1 === 3 val3Javascript.push()
-      //else if result1 === 4 val4Javascript.push()
-    }
-    let val2Java = [];
-    let val3Javascript = [];
-    let val4Python = [];
-
-    //THEN when each Q's answer is pushed into the appropriate array
-
-
-    });
-//    let results = highest # of answers ... or total occurences of x value
-
-//this line of code is for totalling up value inputs once I can figure out how to do that. Must parse value for accurate result!
-//    $("#question1").parseInt(question1Input);
-
-    // if (results) {
-    //   (total # of 1 values > all others)
-    //   then A
-    //   $(this).remove();
-    //   $("#swift-logo").fadeIn();   
-      // else if (2 > all others)
-      // then B
-      // $(this).remove();
-      // $('#java-logo').fadeIn();
-      // else if (3 > all others)
-      // then C
-      // $(this).remove();
-      // $('#javascript-logo').fadeIn();
-      // else if (4 > all others)
-      // $(this).remove();
-      // $('#python-logo').fadeIn();
-      // }
+      const result1 = $("#question1").val();
+        if (result1 === "1") {
+        $(this).remove();
+        $('#swift-logo').show();
+      } else if (result1 === "2") {
+        $(this).remove();
+        $('#java-logo').show();
+      } else if (result1 === "3") {
+        $(this).remove();
+        $('#javascript-logo').show();
+      } else if (result1 === "4") {
+        $(this).remove();
+        $('#python-logo').show();
+      }
   });
-
- //   }
-    //this line of code is also for totalling up all value inputs - once I figure that out.
-    //    $("#quiz-results").show()
+});
--- testing.js
+++ testing.js
@@ -1,18 +1,101 @@
+
+//    let results = highest # of answers ... or total occurences of x value
 //Test1: Get question const to return selected option's value
 //Test1: Success
 //Test 2: Create empty arrays for all 4 coding langs. 
 
 
 
+//quiz.html page  
+
+//  $("form#quiz").submit(function(event) {
+//    event.preventDefault();
+//    const result1 = $("#question1").val();
+//    const result2 = $("#question2").val();
+//    const result3 = $("#question3").val();
+//    const result4 = $("#question4").val(); 
+//    const result5 = $("#question5").val();
+
+// OR could I do one const results = [];
+
+//    let swiftValues = [];
+//    let javaValues = [];
+//    let javascriptValues = [];
+//    let pythonValues = [];
+
+
+//    if (result1 === 1) {
+//      swiftValues.push(result1); 
+//    } else if (result1 === 2) {
+//      javaValues.push(result1); 
+//    } else if (result1 === 3) {
+//      javascriptValues.push(result1);
+//    } else if (result1 === 4) {
+//      pythonValues.push(result1)
+//   }    
+    
+// ----------------------------------
+// .push to javaValues
+//and so on
+  
+
+    // is result1 the right thing to put at the beginning of the next method?
+    // is 'for each' the right thing to use, or should I use for? 
+    // will have to repeat for each Q if it IS right/OK : 
+    //result2.foreach , result3.foreach etc. 
+    // maybe MDN's elements.join(); will be a thing? 
+
+//    result1.forEach(function(value) {
+//    }
+    
+
+    //THEN when each Q's answer is pushed into the appropriate array
+    //val1Swift.length 
+
+
+    // if (results) {
+    //   if val1Swift.length > val2Java.length && val3Javascript.length %% val4Python.length
+    //   then...
+    //   $(this).remove();
+    //   $("#swift-logo").fadeIn(); 
+
+      // else if val2Java.length > other three .length
+      // then...
+      // $(this).remove();
+      // $('#java-logo').fadeIn();
+
+      // else if (3 > all others)
+      // then C
+      // $(this).remove();
+      // $('#javascript-logo').fadeIn();
+
+      // else if (4 > all others)
+      // $(this).remove();
+      // $('#python-logo').fadeIn();
+      // }
+
 //  what if there are even number of A,B,C,D answers returned ??    
 
+      // else if any valx === another valy
+      // then 5th result 
+      // $(('#id1') + ' or ' + ('#id2)').fadeIn;
+      //OR skip and include bug in README
 
+      //else if 0 answers
+      //alert("Please select answers to the questions before submitting.")
+// });
+//   }
+
+
+//---------------------------------------------
+
+// Very 1st iteration of code, pre-refactored, for reference
 
 //    $("form#quiz").submit(function(event) {
 //     event.preventDefault();
 //     const question1 = $("select#question1").val();
-// //this line of code is for totalling up value inputs once I can figure out how to do that. Must parse value for accurate result!
-// //    $("#question1").parseInt(question1Input);
+
+//     $("#question1").parseInt(question1Input);
 //     if (question1 === "1") {
 //       $(this).remove();
 //       $("#swift-logo").fadeIn();
